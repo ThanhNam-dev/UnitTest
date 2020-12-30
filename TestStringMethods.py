bde6868b18f8c69175b3a28f19f08c851f1680f1
ThanhNam-dev <nam05062000@gmail.com>
2020-12-30 16:54:50 +0700

    Main Test01

---
+++ TestStringMethods.py
@@ -0,0 +1,30 @@
+import unittest
+
+from test1 import sum
+from test1 import lengths
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_sum(self):
+        self.assertEqual(sum(1,2),3)
+
+    def test_submit(self):
+        self.assertEqual(lengths("Hiếu là con heo"),15)
+
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test1.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    return a+b
+
+def lengths(text):
+    return len(text)
